eacf96ca394e351df203393a609b78d207520d57
Gedaias <gedaias@geda.dev>
2025-10-06 16:07:05 -0600

    Add password visibility toggler in LoginForm and SignupForm components

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -2,11 +2,13 @@ import { useState } from "react";
 import { useAuth } from "../hooks/useAuth";
 import { useNavigate } from "react-router-dom";
 import { useFormValidations } from "../hooks/useFormValidations";
+import { LuEye, LuEyeClosed } from "react-icons/lu";
 
 export const LoginForm = ({ email, resetEmail }) => {
   const { access } = useAuth();
   const { validateField, errors, cleanSpaces } = useFormValidations();
   const navigate = useNavigate();
+  const [visiblePassword, setVisiblePassword] = useState(false);
   const [formData, setFormData] = useState({
     email: email,
     password: "",
@@ -50,15 +52,23 @@ export const LoginForm = ({ email, resetEmail }) => {
           <label htmlFor="password" className="text-sm text-gray-600">
             Password
           </label>
-          <input
-            id="password"
-            name="password"
-            type="password"
-            placeholder="Enter your password"
-            className="border-none py-2 px-3 rounded-md bg-gray-200 text-sm focus:outline-none"
-            value={formData.password}
-            onChange={handleChange}
-          />
+          <div className="relative">
+            <input
+              id="password"
+              name="password"
+              type={visiblePassword ? "text" : "password"}
+              placeholder="Enter your password"
+              className="border-none py-2 px-3 rounded-md bg-gray-200 text-sm focus:outline-none w-full"
+              value={formData.password}
+              onChange={handleChange}
+            />
+            <div
+              className="absolute top-1/2 -translate-y-1/2 right-0 px-2 cursor-pointer"
+              onClick={() => setVisiblePassword(!visiblePassword)}
+            >
+              {visiblePassword ? <LuEyeClosed /> : <LuEye />}
+            </div>
+          </div>
           {errors?.password && (
             <span className="absolute top-full right-0 bg-rose-500 text-gray-800 text-xs rounded p-1 bg-opacity-95 z-10 max-w-64">
               {errors.password}
