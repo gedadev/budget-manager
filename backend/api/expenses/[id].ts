f53abee860abef18e871c7b08fb683ab0cc317d0
Gedaias <gedaias@geda.dev>
2025-10-17 13:56:56 -0600

    Add API endpoint to delete an expense by ID

---
+++ backend/api/expenses/[id].ts
@@ -0,0 +1,37 @@
+import type { VercelRequest, VercelResponse } from "@vercel/node";
+import { authUser } from "../../middleware/authUser";
+import { getDb } from "../../lib/db";
+import { ObjectId } from "mongodb";
+
+async function handler(req: VercelRequest, res: VercelResponse) {
+  const { userId } = req.body;
+  const { id } = req.query;
+
+  if (req.method === "DELETE") {
+    try {
+      const db = await getDb();
+      const expensesCollection = db.collection("expenses");
+      const usersCollection = db.collection("users");
+
+      const foundUser = await usersCollection.findOne({
+        _id: ObjectId.createFromHexString(userId),
+      });
+
+      if (!foundUser) throw new Error("User not found");
+
+      await expensesCollection.deleteOne({
+        _id: ObjectId.createFromHexString(id as string),
+      });
+
+      return res.status(200).json({ message: "Expense deleted" });
+    } catch (error) {
+      if (error instanceof Error)
+        return res.status(400).json({ error: error.message });
+      return res.status(500).json({ error: "Internal server error" });
+    }
+  }
+
+  res.end();
+}
+
+export default authUser(handler);
