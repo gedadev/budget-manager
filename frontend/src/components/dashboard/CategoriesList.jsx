e365f8a63ae8b8bb966170797d760743b39a67d0
Gedaias <gedaias@geda.dev>
2025-10-21 17:41:11 -0600

    Add category display in CategoriesList with subcategory details and integrate useCategories hook for data fetching

--- frontend/src/components/dashboard/CategoriesList.jsx
+++ frontend/src/components/dashboard/CategoriesList.jsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
 import { CategoryForm } from "./CategoryForm";
-import { LuPlus } from "react-icons/lu";
+import { LuPlus, LuTag } from "react-icons/lu";
+import { useCategories } from "../../hooks/useCategories";
 
 export function CategoriesList() {
   const [activeModal, setActiveModal] = useState(false);
+  const { categories } = useCategories();
 
   const cancelForm = () => {
     setActiveModal(false);
@@ -11,6 +13,35 @@ export function CategoriesList() {
 
   return (
     <section className="w-full grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
+      {categories?.map((category) => (
+        <div
+          key={category._id}
+          className={
+            "flex flex-col justify-between p-2 w-full h-full bg-slate-700 rounded min-h-40"
+          }
+          style={{ borderTop: `solid 0.2rem ${category.color}` }}
+        >
+          <div className="flex gap-2 text-lg">
+            <span>{category.emoji}</span>
+            <span>{category.name}</span>
+          </div>
+          <div>
+            <h3 className="text-slate-300 text-sm py-2 flex items-center gap-1">
+              <LuTag /> Subcategories ({category.subcategories?.length})
+            </h3>
+            <div className="flex gap-1">
+              {category.subcategories?.map((subcategory) => (
+                <span
+                  key={subcategory._id}
+                  className="py-0.5 px-1 rounded-lg border border-slate-500 min-w-fit bg-slate-600 text-sm text-slate-400"
+                >
+                  {subcategory.name}
+                </span>
+              ))}
+            </div>
+          </div>
+        </div>
+      ))}
       <div
         onClick={() => setActiveModal(true)}
         className="flex flex-col items-center justify-center gap-1 cursor-pointer w-full h-full bg-slate-700 rounded min-h-40 border-2 border-slate-500 hover:border-cyan-600 transition-all duration-300 ease-in-out"
--- frontend/src/context/CategoriesContext.jsx
+++ frontend/src/context/CategoriesContext.jsx
@@ -1,10 +1,15 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
 import { useApi } from "../hooks/useApi";
 
 export const CategoriesContext = createContext(null);
 
 export function CategoriesProvider({ children }) {
   const { request, endpoints } = useApi();
+  const [categories, setCategories] = useState();
+
+  useEffect(() => {
+    getCategories();
+  }, []);
 
   async function addCategory(formData) {
     try {
@@ -15,13 +20,26 @@ export function CategoriesProvider({ children }) {
 
       if (newCategory instanceof Error) throw newCategory;
 
+      getCategories();
       return { success: true, message: "Category added" };
     } catch (error) {
       throw error;
     }
   }
 
-  const value = { addCategory };
+  async function getCategories() {
+    try {
+      const categories = await request(endpoints.categories.all);
+
+      if (categories instanceof Error) throw categories;
+
+      setCategories(categories);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  const value = { addCategory, categories };
 
   return (
     <CategoriesContext.Provider value={value}>
