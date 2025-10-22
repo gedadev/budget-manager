9d8afbcc90413c79640d1545f1e851ebc2d56b92
Gedaias <gedaias@geda.dev>
2025-10-21 18:36:22 -0600

    Add Expenses component with header, add expense functionality, and expenses list integration

---
+++ frontend/src/components/dashboard/Expenses.jsx
@@ -0,0 +1,42 @@
+import { Link } from "react-router-dom";
+import { useAuth } from "../../hooks/useAuth";
+import { AddExpense } from "./AddExpense";
+import { ExpensesList } from "./ExpensesList";
+import { LuFolders, LuLogOut } from "react-icons/lu";
+
+export function Expenses() {
+  return (
+    <div>
+      <Header />
+      <AddExpense />
+      <ExpensesList />
+    </div>
+  );
+}
+
+const Header = () => {
+  const { logout } = useAuth();
+
+  return (
+    <header>
+      <div className="flex justify-between items-center mx-auto max-w-6xl h-16 py-4">
+        <div></div>
+        <div>
+          <ul className="flex gap-2">
+            <Link to={"categories"}>
+              <li className="flex items-center gap-2 bg-slate-700 p-2 rounded-md">
+                <LuFolders /> Manage Categories
+              </li>
+            </Link>
+            <li
+              className="cursor-pointer flex items-center gap-2 bg-slate-700 p-2 rounded-md"
+              onClick={logout}
+            >
+              <LuLogOut /> Logout
+            </li>
+          </ul>
+        </div>
+      </div>
+    </header>
+  );
+};
