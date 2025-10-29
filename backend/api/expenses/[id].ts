b528a9e2752a448cc638f36adab09bb6c6d78264
Gedaias <gedaias@geda.dev>
2025-10-29 14:52:03 -0600

    Update API expenses id endpoint to update an expense by Id

--- backend/api/expenses/[id].ts
+++ backend/api/expenses/[id].ts
@@ -31,6 +31,53 @@ async function handler(req: VercelRequest, res: VercelResponse) {
     }
   }
 
+  if (req.method === "PUT") {
+    const {
+      amount,
+      date,
+      commerce,
+      description,
+      categoryId,
+      subcategoryId,
+      method,
+    } = req.body;
+
+    try {
+      const db = await getDb();
+      const expensesCollection = db.collection("expenses");
+      const usersCollection = db.collection("users");
+
+      const foundUser = await usersCollection.findOne({
+        _id: ObjectId.createFromHexString(userId),
+      });
+
+      if (!foundUser) throw new Error("User not found");
+
+      await expensesCollection.updateOne(
+        {
+          _id: ObjectId.createFromHexString(id as string),
+        },
+        {
+          $set: {
+            amount: Number(amount),
+            date: new Date(date),
+            commerce,
+            description,
+            categoryId,
+            subcategoryId,
+            method,
+          },
+        }
+      );
+
+      return res.status(200).json({ message: "Expense updated" });
+    } catch (error) {
+      if (error instanceof Error)
+        return res.status(400).json({ error: error.message });
+      res.status(500).json({ error: "Internal server error" });
+    }
+  }
+
   res.end();
 }
 
