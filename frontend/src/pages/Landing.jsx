ce540dfdd8cba5d5a03be6296b176859b72fb84f
Gedaias <gedaias@geda.dev>
2025-11-09 17:24:20 -0600

    Add navigation buttons for section transitions on landing page, improving user interaction with up and down controls

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -1,6 +1,7 @@
 import { Benefits, CTA, Features, Hero } from "../components/landing/Landing";
 import { Header } from "../components/landing/Header";
 import { useEffect, useRef, useState } from "react";
+import { LuChevronDown, LuChevronUp } from "react-icons/lu";
 
 export function Landing() {
   const landingSections = [<Hero />, <Features />, <Benefits />, <CTA />];
@@ -14,18 +15,8 @@ export function Landing() {
       isLoadingRef.current = true;
       const { deltaY } = e;
 
-      if (deltaY > 0) {
-        setViewIndex((index) => {
-          if (index === landingSections.length - 1) return 0;
-          return index + 1;
-        });
-      }
-      if (deltaY < 0) {
-        setViewIndex((index) => {
-          if (index === 0) return landingSections.length - 1;
-          return index - 1;
-        });
-      }
+      if (deltaY > 0) nextView();
+      if (deltaY < 0) prevView();
 
       setTimeout(() => (isLoadingRef.current = false), 500);
     };
@@ -34,6 +25,18 @@ export function Landing() {
 
     return () => window.removeEventListener("wheel", handleScroll);
   }, []);
+
+  const nextView = () =>
+    setViewIndex((index) => {
+      if (index === landingSections.length - 1) return 0;
+      return index + 1;
+    });
+
+  const prevView = () =>
+    setViewIndex((index) => {
+      if (index === 0) return landingSections.length - 1;
+      return index - 1;
+    });
 
   return (
     <div>
@@ -51,6 +54,24 @@ export function Landing() {
           </div>
         ))}
       </main>
+      <div className="fixed bottom-0 m-5 flex flex-col gap-3">
+        <LuChevronUp
+          onClick={prevView}
+          className={`${
+            viewIndex === 0
+              ? "opacity-0 pointer-events-none"
+              : "opacity-100 pointer-events-auto"
+          } text-slate-400 bg-slate-700 rounded-full p-1 text-4xl cursor-pointer hover:bg-slate-600 transition-all duration-150`}
+        />
+        <LuChevronDown
+          onClick={nextView}
+          className={`${
+            viewIndex === landingSections.length - 1
+              ? "opacity-0 pointer-events-none"
+              : "opacity-100 pointer-events-auto"
+          } text-slate-400 bg-slate-700 rounded-full p-1 text-4xl cursor-pointer hover:bg-slate-600 transition-all duration-150`}
+        />
+      </div>
     </div>
   );
 }
